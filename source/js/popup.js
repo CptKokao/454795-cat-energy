ed2ad9360e4e940a6a34ff8e1fa7dc15a69aa5a6
CptKokao <pro-markov@yandex.ru>
2018-02-13 21:27:50 +0400

    Сделал меню для всех размеров

--- source/js/popup.js
+++ source/js/popup.js
@@ -3,11 +3,11 @@ var navToggle = document.querySelector('.main-nav__btn');
 
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__menu--closed')) {
+    navMain.classList.remove('main-nav__menu--closed');
+    navMain.classList.add('main-nav__menu--opened');
 } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__menu--closed');
+    navMain.classList.remove('main-nav__menu--opened');
   }
 });
