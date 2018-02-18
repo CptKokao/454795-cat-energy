9d8fc85243cebc1a7ee81e99dff5971fc1db50ea
CptKokao <30856643+CptKokao@users.noreply.github.com>
2018-02-18 15:34:53 +0300

    исправил travis

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,106 @@
 
 var gulp = require("gulp");
 var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var plumber = require("gulp-plumber"); //После найденой ошибки (показывает) программа продолжается работать
+var autoprefixer = require('gulp-autoprefixer'); // Подключаем библиотеку для автоматического добавления префиксов
+var server = require("browser-sync"); //запускает сервер
+var minify = require("gulp-csso");  // минимизирует css
+var rename = require("gulp-rename"); // переименовывает файл
+var imagemin = require("gulp-imagemin"); //оптимизирует картинки
+var svgstore = require("gulp-svgstore"); // svg спрайт
+var webp = require("gulp-webp"); //добавляем изображения в формате webp
+// var include = require("posthtml-include"); //
+var del = require("del"); //удаляет папку
+var uglify = require('gulp-uglify'); //минимизирует js
+var pump = require('pump'); //минимизирует js
+var run = require("run-sequence"); //запускает последовательно процессы
 
+//формирует из less => css, минимизирует css
 gulp.task("style", function() {
-  gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+  gulp.src("source/less/style.less") //берем источник
+    .pipe(plumber()) //отлавливает ошибки и не тормозит сервер
+    .pipe(less()) //формирует из less => css
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+    .pipe(gulp.dest("build/css")) //положить в формате css (в папку build для production)
+    .pipe(server.stream()) //говорит что изменился css файл и нужно обновить страницу
+    .pipe(minify()) // минимизирует css
+    .pipe(rename("style.min.css")) // переименовывает файл, чтобы не стерся css gulp.dest("css")
+    .pipe(gulp.dest("build/css")); //положить в формате css мини.файл (в папку build для production)
 });
 
-gulp.task("serve", ["style"], function() {
+//запускает browser-sync, отслеживает файлы
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "source/" // путь до корня сайта (index.html) .тажа самая папка в которой мы находимся
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/less/**/*.less", ["style"]) // файлы за которыми надо следить **любое количесвто подпапок, *любое. Если есть изменения то запускает задачу Style gulp.task("style")
+  gulp.watch("source/*.html", ["html"]) // тоже самое только для html
+});
+
+//оптимизирует картинки, верезает информацию
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}") //форматы с которые нужно оптимизировать
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}), //прогрессивная загрузка
+    imagemin.optipng({optimizationLevel: 3}), //уровень сжатия безопасный
+    imagemin.svgo ()
+  ]))
+  .pipe(gulp.dest("build/img")); //положить в папку img
+});
+
+//создаем svg спрайт
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true  // инлайн свг
+  }))
+  .pipe(rename("sprite.svg")) // переименовывает файл
+  .pipe(gulp.dest("build/img")); //положить спрайт (в папку build для production)
+});
+
+//добавляем изображения в формате webp
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90})) //уровень сжатия
+  .pipe(gulp.dest("img")); //положить в папку img
+});
+
+//минимизирует js
+gulp.task("minjs", function () {
+  return gulp.src("source/js/*.js")
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest("build/js"))
+});
+
+//копирование файлов в папку build (шрифты с расширением, изображения, js)
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+});
+
+//удаляет папку build
+gulp.task("clean", function(){
+  return del("build");
+});
+
+//Запуск "build": "gulp build" в package.json
+gulp.task("build", function (done) {
+  run (
+    "clean",
+    "style",
+    "copy",
+    "minjs",
+    // "images",
+    "sprite",
+    done
+  );
 });
